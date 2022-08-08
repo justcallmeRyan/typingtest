dd050d4c2539ac3445465216ce81b95b810981b7
Fake <jaja_dajaja@hotmail.com>
2022-08-08 16:42:42 +0300

    Add profile dropdown menu in navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -16,6 +16,7 @@ const Navbar = (props) => {
     const {currentUser} = useSelector(state=>state.user)
 
     const [picked, setPicked] = useState("nav-current")
+    const [profileMenu, setProfileMenu] = useState(false)
 
     function handleLogout() {
         navigate("/")
@@ -26,33 +27,52 @@ const Navbar = (props) => {
         dispatch(changeResultDone(false))
     }
 
+    function handleProfile() {
+        setProfileMenu(!profileMenu)
+        console.log(profileMenu)
+    }
+
     return (
+        <>
+            <div className="nav-container">
 
-        <div className="nav-container">
-
-            <Link to={"/"}> <div onClick={handleHome}> <img src="/LogoSmall.svg" /> </div> </Link>
-            <div className="nav-components">
-                <Link to={"/"} style={{textDecoration: 'none', color: 'black'}}>{props.typingtest? <span className="nav-components-spans-picked ">Typing Test</span> : <span className="nav-components-spans ">Typing Test</span>}</Link>
-                <Link to={"/leaderboard"} style={{textDecoration: 'none', color: 'black'}}>{props.leaderboard? <span className="nav-components-spans-picked ">Leaderboard</span> : <span className="nav-components-spans ">Leaderboard</span>}</Link>
-                <Link to={"/about"} style={{textDecoration: 'none', color: 'black'}}> {props.about? <span className="nav-components-spans-picked ">About</span> : <span className="nav-components-spans ">About</span>}</Link>
-            </div>
-            {currentUser? (
-                <div className="nav-profile">
-                    <img className="nav-profile-img" src="/userprofile.svg" />
-                    <Link to={"/profile/" + currentUser.username}> <span> {currentUser.username} </span> </Link>
-                    <button onClick={handleLogout} >Logout</button>
+                <Link to={"/"}> <div onClick={handleHome}> <img src="/LogoSmall.svg" /> </div> </Link>
+                <div className="nav-components">
+                    <Link to={"/"} style={{textDecoration: 'none', color: 'black'}}>{props.typingtest? <span className="nav-components-spans-picked ">Typing Test</span> : <span className="nav-components-spans ">Typing Test</span>}</Link>
+                    <Link to={"/leaderboard"} style={{textDecoration: 'none', color: 'black'}}>{props.leaderboard? <span className="nav-components-spans-picked ">Leaderboard</span> : <span className="nav-components-spans ">Leaderboard</span>}</Link>
+                    <Link to={"/about"} style={{textDecoration: 'none', color: 'black'}}> {props.about? <span className="nav-components-spans-picked ">About</span> : <span className="nav-components-spans ">About</span>}</Link>
                 </div>
-            ): (<div className="nav-login-register">
-                <Link to={"/login"} ><button className="nav-login" >Log in</button></Link>
-                <span> | </span>
-                <Link to={"/register"}><button className="nav-register">Register </button></Link>
-            </div>)}
+                {currentUser? (
+                    <div className="nav-profile">
+                        <img onClick={handleProfile} className="nav-profile-img" src="/userprofile.svg" />
+                        <span className="nickname"> {currentUser.username} </span>
+                        {profileMenu&&<> <div className="profile-menu-container">
+
+                            <Link  to={"/profile/" + currentUser.username} style={{textDecoration: 'none', color: 'black'}}><div className="profile-menu-item">
+                                <img className="profile-imgs" src="/user1.svg" />
+                                 <span>My profile</span>
+                            </div></Link>
+                            <div className="profile-line"></div>
+                            <div className="profile-menu-item">
+                                <img className="profile-imgs" src="/power1.svg" />
+                                <button className="profile-menu-button" onClick={handleLogout} >Logout</button>
+                            </div>
 
 
+                        </div>
+                            </>}
 
 
+                    </div>
+                ): (<div className="nav-login-register">
+                    <Link to={"/login"} ><button className="nav-login" >Log in</button></Link>
+                    <span> | </span>
+                    <Link to={"/register"}><button className="nav-register">Register </button></Link>
+                </div>)}
+            </div>
 
-        </div>
+        </>
+
     );
 };
 
